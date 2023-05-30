1ce7d7eeeb186454e111d42afd3849decbd965c9
huynguyen <huynguyen@myg.vn>
2023-05-30 14:49:39 +0700

    emit all messages

--- src/components/Events/EventsCard/ListMessages.jsx
+++ src/components/Events/EventsCard/ListMessages.jsx
@@ -1,13 +1,21 @@
 import { useContext } from "react";
 import { SettingsContext } from "../../../contexts/SettingsContext";
+import { SocketContext } from "../../../contexts/SocketContext";
+import { ListMessagesConext } from "../../../contexts/ListMessagesContext";
 import { TextField, IconButton, Tooltip } from "@mui/material";
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MessageDetailCard from "./MessageDetailCard";
+import UploadIcon from '@mui/icons-material/Upload';
+import generateArray from "../../../ultils/generateArray";
 
 export default function ListMessages({ eventIndex, setEventIndex }) {
 
     const { settings, updateSettings } = useContext(SettingsContext);
+
+    const { socket, isConnected } = useContext(SocketContext);
+
+    const {listMessages, updateListMessages} = useContext(ListMessagesConext);
 
     let eventName = settings.events[eventIndex];
 
@@ -61,11 +69,35 @@ export default function ListMessages({ eventIndex, setEventIndex }) {
         saveMessagesInLocalStorage();
     }
 
+    const emitAllMessages = () => {
+        if (!isConnected) return;
+        let eventMessages = settings.messages[eventName];
+        eventMessages.forEach((message) => {
+            let parsedMessage = message.map(item => {
+                try {
+                    return JSON.parse(item);
+                }
+                catch (err) {
+                    return item;
+                }
+            })
+            socket.emit(eventName, ...parsedMessage);
+            updateListMessages([{isEmit: true, eventName, message}, ...listMessages]);
+        });
+    }
+
+    const data = generateArray([
+        ["Add Message", "top-start", <PostAddIcon />, addMessage],
+        ["Emit All Messages", "top-start", <UploadIcon />, emitAllMessages],
+        ["Delete Event", "top-start", <DeleteIcon />, deleteEvent]
+    ], "title", "placement", "icon", "handleClick");
+
     if (eventIndex < 0) return <></>;
 
     return (
         <div>
             <div className="flex h-1/3">
+
                 <TextField
                     size="small"
                     fullWidth
@@ -73,7 +105,20 @@ export default function ListMessages({ eventIndex, setEventIndex }) {
                     value={settings.events[eventIndex]}
                     onChange={updateEvent}
                 />
-                <Tooltip
+                {
+                    data.map((item, index) => (
+                        <Tooltip
+                            title={item.title}
+                            placement={item.placement}
+                            key={index}
+                        >
+                            <IconButton onClick={item.handleClick}>
+                                {item.icon}
+                            </IconButton>
+                        </Tooltip>
+                    ))
+                }
+                {/* <Tooltip
                     title="Add Message"
                     placement="top-start"
                 >
@@ -88,8 +133,8 @@ export default function ListMessages({ eventIndex, setEventIndex }) {
                     <IconButton onClick={deleteEvent}>
                         <DeleteIcon />
                     </IconButton>
-                </Tooltip>
-                
+                </Tooltip> */}
+
             </div>
             <hr className="my-3 border-neutral-500" />
             <div className="flex flex-wrap overflow-auto">
--- src/contexts/SettingsContext.jsx
+++ src/contexts/SettingsContext.jsx
@@ -6,8 +6,7 @@ export const SettingsContext = createContext({
         configs: {},
         tokens: [],
         events: [],
-        args: {},
-        json: {},
+        messages: {},
     },
     updateSettings: (settings) => { }
 });
